d6955d2662178a3ba04f9cc1d04a3ee125f9261e
Paul Pick <ppickone@outlook.com>
2023-04-18 13:31:35 +0100

    Background set
    made a prefab of all the background objects to make one complete image

--- SPACE-SHOOTER/Assets/Scripts/PlayerController.cs
+++ SPACE-SHOOTER/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    [SerializeField] float speed;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
